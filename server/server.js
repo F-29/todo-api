343d8b4091277f2537db34f369c466b1a10440ed
Mosich <m.chegeni7979@gmail.com>
2020-01-03 17:01:15 +0330

    adding GET /todos/:id route (which finds docs by thire ID).

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
-// const mongoose = require('../mongoose-config');
 const Todo = require('./models/todoModel');
-// const User = require('./models/userModel');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 
 app.use(bodyParser.json());
 
@@ -23,6 +22,24 @@ app.get('/todos', (req, res) => {
     Todo.find()
         .then(todos => res.send({todos}))
         .catch(e => res.status(400).send(e));
+});
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(400).send();
+    }
+
+    Todo.findById(id)
+        .then(todo => {
+            if (!todo) {
+                return res.status(404).send();
+            }
+
+            res.send({todo})
+        })
+        .catch(err => res.status(400).send(err));
 });
 
 app.listen(PORT, () => {
