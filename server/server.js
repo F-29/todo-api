5d946f8da13f26f4ece26bf80b2e97c6f4fa9939
Mosich <m.chegeni7979@gmail.com>
2020-01-11 00:46:58 +0330

    add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -42,6 +42,32 @@ app.get('/todos/:id', (req, res) => {
         .catch(err => res.status(400).send());
 });
 
+app.delete('/todos/:id', (req, res) => {
+    let id = checkId(req, res);
+
+    Todo.findByIdAndRemove(id)
+        .then(todo => {
+            if (!todo) {
+                return res.status(404).send();
+            }
+            console.log("removed todo:\n", todo);
+            return res.send(todo);
+        })
+        .catch(e => {
+            console.log("error");
+            return res.status(400).send();
+        });
+});
+
+checkId = (req, res) => {
+    let id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(400).send();
+    }
+
+    return id;
+};
+
 app.listen(PORT, () => {
     console.log(`server is running on localhost:${PORT}`);
 });
